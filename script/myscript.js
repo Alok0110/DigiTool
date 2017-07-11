69596bebd4e89e680a97cc3dbae85d65f877c89e
Alok Dethe <dethealok@gmail.com>
2017-07-11 18:00:48 +1200

    recent js changes

--- script/myscript.js
+++ script/myscript.js
@@ -2,7 +2,6 @@ var sbobject = $SB( { image: true, setImageAsBackground: true,topMargin: "0px" }
 //var sbobject2 = $SB( { image: false } );
 console.log( " this is the object ==> "+JSON.stringify(sbobject) );
 //sbobject.testMethod();
-sbobject.obj.image=true;
 //sbobject.testMethodTwo().obj.image=false;
 //sbobject.testMethod();
 //console.log("got through method chaining ==> "+sbobject.testMethodTwo().obj.image+" "+sbobject.obj.image);
