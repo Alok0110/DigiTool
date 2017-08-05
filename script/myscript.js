136dc716ebf0dd85b7864c086ff9023384bb1f58
Alok Dethe <dethealok@gmail.com>
2017-08-05 15:01:18 +1200

    recent changes

--- script/myscript.js
+++ script/myscript.js
@@ -1,6 +1,6 @@
-//var sbobject = $SB({ setImageAsBackground:false, topMargin:'70px' });
-var testObj2 = $SB({});
+var sbobject = $SB({ setImageAsBackground:false, topMargin:'70px' });
+//var testObj2 = $SB({});
 //console.log( " this is the object ==> "+JSON.stringify(sbobject) );
 
-//sbobject.setAutomaticScroll(true);
-//sbobject.setAutoNavPanelScroll(true);
+sbobject.setAutomaticScroll(true);
+sbobject.setAutoNavPanelScroll(true);
